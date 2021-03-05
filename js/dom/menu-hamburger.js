c96cce8185b69fe310d141b255161652e162f215
EzeVasari <49240195+EzeVasari@users.noreply.github.com>
2021-03-05 13:43:51 -0300

    Mejora funcion cambiarBtn

--- js/dom/menu-hamburger.js
+++ js/dom/menu-hamburger.js
@@ -4,26 +4,22 @@ export default function hamburgerMenu(menuBtn, panel, menuLink, icon) {
   d.addEventListener("click", (e) => {
     if (e.target.matches(menuBtn) || e.target.matches(`${menuBtn} *`)) {
       d.querySelector(panel).classList.toggle("visible");
-      if (icon.classList.contains("fa-bars")) {
-        icon.classList.remove("fa-bars");
-        icon.classList.add("fa-times");
-      } else {
-        icon.classList.remove("fa-times");
-        icon.classList.add("fa-bars");
-      }
+      cambiarBtn(icon);
     }
 
     if (e.target.matches(menuLink)) {
       d.querySelector(panel).classList.remove("visible");
-      if (icon.classList.contains("fa-bars")) {
-        icon.classList.remove("fa-bars");
-        icon.classList.add("fa-times");
-      } else {
-        icon.classList.remove("fa-times");
-        icon.classList.add("fa-bars");
-      }
+      cambiarBtn(icon);
     }
   });
 }
 
-const cambiarBtn = (icon) => {};
+const cambiarBtn = (icon) => {
+  if (icon.classList.contains("fa-bars")) {
+    icon.classList.remove("fa-bars");
+    icon.classList.add("fa-times");
+  } else {
+    icon.classList.remove("fa-times");
+    icon.classList.add("fa-bars");
+  }
+};
