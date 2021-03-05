1dd528176f4c1ae1595720ec41e82ba750ba6f2c
EzeVasari <49240195+EzeVasari@users.noreply.github.com>
2021-03-05 19:39:27 -0300

    Arregla mostrar cruz en menu btn

--- js/dom/menu-hamburger.js
+++ js/dom/menu-hamburger.js
@@ -15,11 +15,13 @@ export default function hamburgerMenu(menuBtn, panel, menuLink, icon) {
 }
 
 const cambiarBtn = (icon) => {
-  if (icon.classList.contains("fa-bars")) {
-    icon.classList.remove("fa-bars");
-    icon.classList.add("fa-times");
+  const $icon = document.querySelector(icon);
+
+  if ($icon.classList.contains("fa-bars")) {
+    $icon.classList.remove("fa-bars");
+    $icon.classList.add("fa-times");
   } else {
-    icon.classList.remove("fa-times");
-    icon.classList.add("fa-bars");
+    $icon.classList.remove("fa-times");
+    $icon.classList.add("fa-bars");
   }
 };
