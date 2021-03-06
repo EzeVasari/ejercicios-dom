8cd459dee01324b24cc99e70508d7ab9dc21ba8a
EzeVasari <49240195+EzeVasari@users.noreply.github.com>
2021-03-05 21:15:48 -0300

    Ejercicio de reloj y alarma terminado

---
+++ js/dom/reloj-alarma.js
@@ -0,0 +1,37 @@
+export default function relojAlarma(clock, clockBtn, alarmBtn, alarmAudio) {
+  const d = document;
+
+  d.addEventListener("click", (e) => {
+    if (e.target.matches(clockBtn)) {
+      d.querySelector(clock).classList.toggle("visible");
+
+      let $relojBtn = d.querySelector(clockBtn);
+
+      if ($relojBtn.textContent == "Iniciar reloj") {
+        $relojBtn.textContent = "Detener reloj";
+      } else {
+        $relojBtn.textContent = "Iniciar reloj";
+      }
+
+      setInterval(() => {
+        d.querySelector(clock).textContent = new Date().toLocaleTimeString();
+      }, 0);
+    }
+
+    if (e.target.matches(alarmBtn)) {
+      if (d.querySelector(alarmAudio).paused) {
+        d.querySelector(alarmAudio).play();
+      } else {
+        d.querySelector(alarmAudio).pause();
+      }
+
+      let $alarmBtn = d.querySelector(alarmBtn);
+
+      if ($alarmBtn.textContent == "Iniciar alarma") {
+        $alarmBtn.textContent = "Detener alarma";
+      } else {
+        $alarmBtn.textContent = "Iniciar alarma";
+      }
+    }
+  });
+}
--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 import hamburgerMenu from "./dom/menu-hamburger.js";
+import relojAlarma from "./dom/reloj-alarma.js";
+
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
   hamburgerMenu(".menu-btn", ".panel", "nav a", ".menu-btn i");
+  relojAlarma("#reloj", "#clock-btn", "#alarm-btn", "#alarm-audio");
 });
